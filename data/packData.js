fe36a3eb2835556c1bd2d04feb32aa19184662ec
madeleine-lloyd-davies <mc.lloyd.davies@gmail.com>
2023-01-22 22:31:08 -0500

    chore: added structured list wizard array for custom list questionnaire

--- data/packData.js
+++ data/packData.js
@@ -109,3 +109,170 @@ export const sampleArchive = [
     description: "a second sample archive item",
   },
 ];
+
+//array of questions/categories. each one is an object with a category name and an array of individual items
+
+/*
+how will quantity work?
+most items - default quant 1
+some items - default quant "travelDays"
+some items - default quant "tripDuration"
+set default quantity for each item and have + and - available so user can adjust
+ */
+export const customListWizard = [
+  {
+    name: "Essentials",
+    items: [
+      { name: "wallet", quantity: 1 },
+      { name: "keys", quantity: 1 },
+      { name: "phone", quantity: 1 },
+      { name: "tickets", quantity: "travelDays" },
+      { name: "sufficient ID", quantity: 1 },
+      { name: "payment method", quantity: 1 },
+    ],
+  },
+  {
+    name: "Clothing and Accessories",
+    items: [
+      { name: "travel outfit", quantity: "travelDays" },
+      { name: "daytime outfit", quantity: "tripDuration" },
+      { name: "sleepwear", quantity: 1 },
+      { name: "underwear", quantity: "tripDuration" },
+      { name: "socks", quantity: "tripDuration" },
+      { name: "outerwear (heavy)", quantity: 1 },
+      { name: "outerwear (light)", quantity: 1 },
+      { name: "bag/purse", quantity: 1 },
+      { name: "hat", quantity: 1 },
+      { name: "belt", quantity: 1 },
+      { name: "sunglasses", quantity: 1 },
+      { name: "jewelry set", quantity: 1 },
+      { name: "formal wear", quantity: 1 },
+      { name: "swimwear", quantity: 1 },
+      { name: "exercise outfit", quantity: 1 },
+      { name: "sports equipment", quantity: 1 },
+    ],
+  },
+  {
+    name: "Shoes",
+    items: [
+      { name: "sneakers", quantity: 1 },
+      { name: "hiking boots", quantity: 1 },
+      { name: "formal shoes", quantity: 1 },
+      { name: "slippers", quantity: 1 },
+      { name: "shower shoes", quantity: 1 },
+    ],
+  },
+  {
+    name: "Toiletries",
+    items: [
+      { name: "toothbrush", quantity: 1 },
+      { name: "toothpaste", quantity: 1 },
+      { name: "dental floss", quantity: 1 },
+      { name: "shower items", quantity: 1 },
+      { name: "moisturizer/skincare", quantity: 1 },
+      { name: "sunscreen", quantity: 1 },
+      { name: "menstrual products", quantity: 1 },
+      { name: "sexual health items", quantity: 1 },
+      { name: "deodorant", quantity: 1 },
+      { name: "bugspray", quantity: 1 },
+      { name: "glasses/contacts", quantity: 1 },
+      { name: "shaving equipment", quantity: 1 },
+      { name: "hair products", quantity: 1 },
+      { name: "makeup", quantity: 1 },
+      { name: "perfume", quantity: 1 },
+    ],
+  },
+  {
+    name: "Medication",
+    items: [
+      { name: "prescription medication (daily)", quantity: "tripDuration" },
+      { name: "prescription medication (as needed)", quantity: 1 },
+      { name: "vitamins", quantity: "tripDuration" },
+      { name: "first aid", quantity: 1 },
+      { name: "painkillers", quantity: 1 },
+      { name: "medication for stomach issues", quantity: 1 },
+      { name: "nausea kit", quantity: 1 },
+      { name: "melatonin", quantity: 1 },
+    ],
+  },
+  {
+    name: "Daily Items",
+    items: [
+      { name: "water bottle", quantity: 1 },
+      { name: "reuseable bag", quantity: 1 },
+      { name: "notebook", quantity: 1 },
+      { name: "pen/pencil", quantity: 1 },
+      { name: "gum", quantity: 1 },
+    ],
+  },
+  {
+    name: "Tech/Electronic Items",
+    items: [
+      { name: "laptop (and charger)", quantity: 1 },
+      { name: "tablet (and charger)", quantity: 1 },
+      { name: "eReader (and charger)", quantity: 1 },
+      { name: "portable gaming device (and charger)", quantity: 1 },
+      { name: "bluetooth speaker (and charger)", quantity: 1 },
+      { name: "camera equipment", quantity: 1 },
+      { name: "power converter", quantity: 1 },
+      { name: "cables/equipment to connect computer to tv", quantity: 1 },
+    ],
+  },
+  {
+    name: "Accessibility",
+    items: [
+      { name: "cane", quantity: 1 },
+      { name: "walker", quantity: 1 },
+      { name: "wheelchair", quantity: 1 },
+      { name: "hearing aids", quantity: 1 },
+      { name: "plane seatbelt extender", quantity: 1 },
+    ],
+  },
+  {
+    name: "Food",
+    items: [
+      { name: "snacks for travel days", quantity: "travelDays" },
+      { name: "snacks for daily use", quantity: "tripDuration" },
+      { name: "groceries", quantity: 1 },
+    ],
+  },
+  {
+    name: "Entertainment",
+    items: [
+      { name: "books", quantity: 1 },
+      { name: "DVDs", quantity: 1 },
+      {
+        name: "digital media (music, movies, podcasts, ebooks, audiobooks",
+        quantity: 1,
+      },
+      { name: "boardgames/deck of cards", quantity: 1 },
+      { name: "stamps", quantity: 1 },
+    ],
+  },
+
+  {
+    name: "Comfort Items",
+    items: [
+      { name: "sleep mask", quantity: 1 },
+      { name: "travel pillow", quantity: 1 },
+      { name: "earplugs", quantity: 1 },
+      { name: "bed linens", quantity: 1 },
+      { name: "towels", quantity: 1 },
+    ],
+  },
+  {
+    name: "Day Of",
+    items: [
+      { name: "set out travel outfit" },
+      { name: "pack items that you were still using / for top of bag" },
+      { name: "take out garbage" },
+      { name: "check into flight" },
+      { name: "contact friends/family with update" },
+      { name: "download media" },
+      { name: "schedule ride to airport" },
+      { name: "check in with pet sitter" },
+      { name: "clear out fridge" },
+      { name: "set autoresponder in your email" },
+    ],
+  },
+];
