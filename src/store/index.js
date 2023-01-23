e89bf14e4e0fd006a48a8ebc316cf0e978c3e4a4
madeleine-lloyd-davies <mc.lloyd.davies@gmail.com>
2023-01-22 21:32:52 -0500

    feat: import new custom list slice to store index

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import packListReducer from "./slices/packListSlice";
+import customListReducer from "./slices/customListSlice";
 
 const store = configureStore({
   reducer: {
     packList: packListReducer,
+    customList: customListReducer,
   },
 });
 
