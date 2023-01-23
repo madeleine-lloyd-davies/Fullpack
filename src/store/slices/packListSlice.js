f21ef9c281953fc482655b7ce5318a5d8edbe2aa
madeleine-lloyd-davies <mc.lloyd.davies@gmail.com>
2023-01-22 19:42:52 -0500

    feat: add reducers for basic list to slice page

--- src/store/slices/packListSlice.js
+++ src/store/slices/packListSlice.js
@@ -14,8 +14,29 @@ const packListSlice = createSlice({
       //3. push it to complete array
       state.complete.push(action.payload);
     },
+    unarchive: (state, action) => {
+      //1. action.payload should be one object that user is unarchiving
+      //2. remove it from inactive array
+      state.inactive = state.inactive.filter(
+        (obj) => obj.name !== action.payload.name
+      );
+      //3. push it to toDo array
+      state.toDo.push(action.payload);
+    },
+    uncomplete: (state, action) => {
+      state.complete = state.complete.filter(
+        (obj) => obj.name !== action.payload.name
+      );
+      state.toDo.push(action.payload);
+    },
+    archive: (state, action) => {
+      state.toDo = state.toDo.filter((obj) => obj.name !== action.payload.name);
+      state.inactive.push(action.payload);
+    },
   },
 });
 
 export const selectPackList = (state) => state.packList;
 export default packListSlice.reducer;
+export const { completeToDo, unarchive, uncomplete, archive } =
+  packListSlice.actions;
